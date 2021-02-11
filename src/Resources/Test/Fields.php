43b1462e7ff74388d7db4c3997fb716f6db8fa41
Andrey Martynov <a.martinov@ddemo.ru>
2021-02-11 14:42:43 +0500

    test resource

---
+++ src/Resources/Test/Fields.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FastDog\Adm\Resources\Test;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Fields
+ * @package FastDog\Adm\Resources\Test
+ */
+class Fields extends Model
+{
+    /** @var string  */
+    public $table = 'test_field';
+}
