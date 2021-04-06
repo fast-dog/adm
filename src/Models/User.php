5c898d97c8edc0cabb44f1fe76834a64804970d3
Andrey Martynov <a.martinov@ddemo.ru>
2021-04-06 21:08:08 +0500

    dev

---
+++ src/Models/User.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace FastDog\Adm\Models;
+
+use Dg482\Red\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
+/**
+ * Class User
+ * @package FastDog\Adm\Models
+ */
+class User extends Authenticatable implements Model
+{
+    use HasFactory, Notifiable, HasRoles;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Fields model
+     * @return array
+     */
+    public function getFields(): array
+    {
+        return [];
+    }
+
+    /**
+     * @param  array  $attributes
+     * @param  array  $options
+     * @return bool
+     */
+    public function updateModel(array $attributes, array $options = [])
+    {
+        return $this->update($attributes, $options);
+    }
+}
---
+++ src/Resources/User/Forms/Identity.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace FastDog\Adm\Resources\User\Forms;
+
+
+use Dg482\Red\Builders\Form\BaseForms;
+use Dg482\Red\Builders\Form\FormModelInterface;
+use Dg482\Red\Builders\Form\Structure\Fieldset;
+use Exception;
+use FastDog\Adm\Models\User;
+
+/**
+ * Class Profile
+ * @package App\Models\Forms\User
+ */
+class Identity extends BaseForms implements FormModelInterface
+{
+    /** @var string */
+    public string $title = 'Данные пользователя';
+
+    /** @var string */
+    protected string $formName = 'user/identity';
+
+    /**
+     * Identity constructor.
+     * @param  User  $model
+     */
+    public function __construct(User $model)
+    {
+        $this->setModel($model);
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public function resourceFields(): array
+    {
+        return [
+            Fieldset::make('Основное', '')->setItems($this->fields()),
+        ];
+    }
+}
