05764ee0285efe15a2a1e1ee0295bb06a940066e
Andrey Martynov <a.martinov@ddemo.ru>
2021-04-17 10:01:06 +0500

    add localization

--- lang/ru/resources.php
+++ lang/ru/resources.php
@@ -1,10 +1,18 @@
 <?php
 return [
     'user' => [
+        'title' => 'Пользователи',
         'forms' => [
             'identity' => [
                 'title' => 'Данные пользователя',
                 'general' => 'Основное',
+                'fields' => [
+                    'email' => 'Email',
+                    'name' => 'Имя',
+                    'status' => 'Статус',
+                    'role' => 'Привилегии',
+                    'password' => 'Пароль',
+                ],
             ],
         ],
     ],
